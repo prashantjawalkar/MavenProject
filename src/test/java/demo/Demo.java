1eefd4c6eba94edbc411db898cf97347b9da651d
prashantjawalkar <pbjawalkar@gmail.com>
2019-02-09 13:30:47 +0530

    Initial

---
+++ src/test/java/demo/Demo.java
@@ -0,0 +1,28 @@
+package demo;
+
+import javax.sql.rowset.WebRowSet;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class Demo {
+	@Test
+	public void testCase() {
+		System.out.println("TestNG === Maven === Jenkins");
+		for (int i = 1; i <= 10; i++) {
+			System.out.println("I ==== " + i);
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		System.setProperty("webdriver.gecko.driver", "D:\\geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+		driver.get("http://www.facebook.com");
+		System.out.println("Title ====>"+driver.getTitle());
+		driver.quit();
+	}
+}
